4f7a253f77c38d9458ad59d2fba35c91bc9b5a28
anushak007 <anushakulkarni007@gmail.com>
2026-01-17 09:41:23 +0530

    Update test_simple.py

--- test_simple.py
+++ test_simple.py
@@ -1,9 +1,10 @@
 from simple import si
 
-def test_si():
-    assert si(10,5,2) == 1
-def test_si():
-    assert si(10,5,4) == 2
-def test_si():
-    assert si(10,10,3) == 3
-            
+def test_si_1():
+    assert si(10, 5, 2) == 1
+
+def test_si_2():
+    assert si(10, 5, 4) == 2
+
+def test_si_3():
+    assert si(10, 10, 3) == 3
