22dfecd2ce311d1961f5ea3d5b8f76b6548eca21
anushak007 <anushakulkarni007@gmail.com>
2026-01-17 09:33:59 +0530

    Update simple.py

--- simple.py
+++ simple.py
@@ -5,9 +5,9 @@ def si(p,t,r):
 
 if __name__ == "__main__":
     if len(sys.argv)==3:
-        p = float(sys.argv[1])
-        t = float(sys.argv[2])
-        r = float(sys.argv[3])
+        p = int(sys.argv[1])
+        t = int(sys.argv[2])
+        r = int(sys.argv[3])
         
     else:
         p = 100
